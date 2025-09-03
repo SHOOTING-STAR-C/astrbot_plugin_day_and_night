e5c8b29a767a897bee9419c4eb0000e8533c6c44
SHOOTING_STAR_C <944153243@qq.com>
2025-09-03 21:03:14 +0800

    fix:修改数据库字段约束

--- database/sql/day_and_night_tool_plugin_init.sql
+++ database/sql/day_and_night_tool_plugin_init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS user_sleep_records
 (
     user_id INTEGER NOT NULL,              -- 用户ID
-    sleep_time DATETIME NOT NULL,          -- 入睡时间
+    sleep_time DATETIME,                   -- 入睡时间
     wake_time DATETIME,                    -- 醒来时间
     status_date DATE NOT NULL,             -- 统计日期（联合主键部分）
 
